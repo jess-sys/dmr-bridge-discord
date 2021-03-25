1d093ce1deb073cb62bb2c36510d3a0601101def
Alexandre Monier <alexandre.monier@epitech.eu>
2021-03-25 23:53:56 +0100

    logging

--- analog/transmitter.js
+++ analog/transmitter.js
@@ -1,8 +1,8 @@
 const stream = require('stream');
 const dgram = require('dgram');
 
-function send_audio() {
-    const socket = dgram.createSocket({ type: 'udp4', reuseAddr: true });
+function create_tx_socket() {
+    const socket = dgram.createSocket({ type: 'udp4' });
     socket.connect(Number(process.env.DMR_TARGET_RX_PORT), process.env.DMR_TARGET);
 
 }
