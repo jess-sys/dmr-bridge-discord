31468a0689b1ce6228ea76463d373e84b7abb814
Alexandre Monier <alexandre.monier@epitech.eu>
2021-03-25 23:32:36 +0100

    Trying

--- analog/receiver.js
+++ analog/receiver.js
@@ -5,18 +5,16 @@ const OpusScript = require("opusscript");
 const logger = require('../helpers/logger');
 
 function create_rx_socket(connection) {
-    //const encoder = new OpusScript(8000, 1, OpusScript.Application.VOIP);
+    const encoder = new OpusScript(8000, 1, OpusScript.Application.VOIP);
     const socket = dgram.createSocket({ type: 'udp4', recvBufferSize: 320 });
     let queueBuffer = [];
     let garbageListener = setTimeout(() => {
         if (queueBuffer.length === 0)
             return;
-        const queueBufferCopy = queueBuffer;
-        const bufferStream = stream.Readable.from(queueBufferCopy);
+        const opusBuffer = queueBuffer.map((buffer) => encoder.encode(buffer, 160));
+        const opusStream = stream.Readable.from(opusBuffer);
         logger.info('RX', 'PTT', 'PTT button released. Pushing audio frame of size ' + queueBuffer.reduce((acc, buf) => acc + buf.length), 0);
-        //const opusBuffer = encoder.encode(buffer, buffer.length / 2);
-        //const opusStream = stream.Readable.from(opusBuffer);
-        connection.play(bufferStream);
+        connection.play(opusStream, { type: 'opus' });
         queueBuffer = [];
     }, 150);
 
