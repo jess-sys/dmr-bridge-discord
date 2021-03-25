b81c22ed51ac37fa20b7ff95376fcafc314107d4
Alexandre Monier <alexandre.monier@epitech.eu>
2021-03-25 20:25:37 +0100

    debug

--- analog/receiver.js
+++ analog/receiver.js
@@ -52,7 +52,17 @@ function create_rx_socket(connection) {
         if (rinfo.address !== process.env.DMR_TARGET || rinfo.size !== 352)
             return;
         const { header, eye, seq, memory, keyup, talkgroup, type, mpxid, reserved, audio } = parse_receiver_data(msg);
-        console.log(header, eye, seq, memory, keyup, talkgroup, type, mpxid, reserved)
+        console.log({
+            "header": header, 
+            "eye": eye, 
+            "seq": seq, 
+            "memory": memory, 
+            "keyup": keyup, 
+            "talkgroup": talkgroup, 
+            "type": type, 
+            "mpxid": mpxid, 
+            "reserved": reserved
+        })
         if (header?.toString('ascii') === 'USRP') {
             if (type == 0) {
                 const opusBuffer = encoder.encode(audio, 160);
