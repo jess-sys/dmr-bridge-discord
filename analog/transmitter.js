76d4e2eff39e7c2db3813c41b808212d3a4bde27
Alexandre Monier <alexandre.monier@epitech.eu>
2021-03-25 17:51:53 +0100

    Fix exports

--- analog/transmitter.js
+++ analog/transmitter.js
@@ -1,8 +1,12 @@
 const stream = require('stream');
 const dgram = require('dgram');
 
-export function send_audio() {
+function send_audio() {
     const socket = dgram.createSocket({ type: 'udp4', reuseAddr: true });
     socket.connect(Number(process.env.DMR_TARGET_RX_PORT), process.env.DMR_TARGET);
 
+}
+
+module.exports = {
+    send_audio
 }
